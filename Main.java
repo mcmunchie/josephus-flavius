f1032ae2ee7c1f374acb4c8512a9f66de0e6e4ef
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-20 02:07:00 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
         Josephus fJosephus = new Josephus();
         fJosephus.josephusFlaviusProblem();
+        System.out.println();
     }
 }
